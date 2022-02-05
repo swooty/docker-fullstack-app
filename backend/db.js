c7827883bdec1c4bab7b70c8d06a9f672be47f9a
JM.Son <swoosnoo@gmail.com>
2022-02-05 16:51:31 +0900

    travis ci deploy Added

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,17 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
     connectionLimit: 10,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
+});
+/*const pool = mysql.createPool({
+    connectionLimit: 10,
     host: 'mysql',
     user: 'root',
-    password: 'sonjm',
+    password: 'sonjm', // 8자 이상되어야 함
     database: 'myapp'
-});
+});*/
 exports.pool = pool;
